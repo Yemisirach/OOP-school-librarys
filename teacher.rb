15449aba32b8d9836d9e8f0fc5a7b4f3ff7b9261
ambrose-kibet <ambrosekibet36@gmail.com>
2023-04-25 18:29:47 +0300

    add student and techer classes

---
+++ teacher.rb
@@ -0,0 +1,13 @@
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(name, age, specialization, parent_permission: true)
+    super(name, age, parent_permission)
+
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
