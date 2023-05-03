c7c2ce439b58014b99811083c629c7667b496854
ambrose-kibet <ambrosekibet36@gmail.com>
2023-05-03 12:07:36 +0300

    add test for classrom

--- spec/classroom_test.rb
+++ spec/classroom_test.rb
@@ -5,18 +5,18 @@ require_relative "../class/student.rb"
 describe Classroom do
   before(:each) do
     @classroom = Classroom.new('Maths')
-    @student = Student.new(26,  'Ermiyas')
+    @student = Student.new(26,  'esada')
   end
 
-  describe '#new' do
-    it 'returns a classroom object' do
+  describe 'Classroom' do
+    it 'checks if its an instance of classroom' do
       expect(@classroom).to be_an_instance_of(Classroom)
     end
   end
 
-   describe '#add_students' do
-    context 'when the student' do
-      it 'checks if the student has a classroom' do
+   describe 'Can Add students' do
+    context 'With valid input' do
+      it 'checks if the classroom contains student' do
         @classroom.add_student(@student)
         expect(@classroom.students).to include(@student)
       end
